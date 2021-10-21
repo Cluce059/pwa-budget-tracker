7aa9eeb7c94815a5a95d3023e923e38dbea39382
caroline <cluce059@gmail.com>
2021-10-20 20:15:39 -0400

    updated unitopo param

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,14 +5,14 @@ request.onupgradeneeded = function(event) {
     // save a ref to the db 
     const db = event.target.result;
     // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('waiting', { autoIncrement: true }); //waiting for obj store
+    db.createObjectStore('pending', { autoIncrement: true }); //waiting for obj store
   };
 
 request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // check if app is online, if yes run /checkdb() function to send all local db data to api
     if (navigator.onLine) {
       // we haven't created this yet, but we will soon, so let's comment it out for now
       checkDb();
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,21 @@
-const APP_PREFIX  = 'BudgetTracker-',
-const VERSION = 'version_01',
+const APP_PREFIX  = 'BudgetTracker-';
+const VERSION = 'version_01';
 const CACHE_NAME  = APP_PREFIX+VERSION;
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/styles.css",
-    "./js/index.js",
-    "./manifest.webmanifest",
-    "./icons/icon-192x192.png",
-    "./icons/icon-512x512.png",
-    "./js/idb.js"
+    "/index.html",
+    "/css/styles.css",
+    "/js/index.js",
+    "/manifest.webmanifest",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png",
+    "/js/idb.js"
 ];
 
 //install service-worker
@@ -44,17 +50,43 @@ self.addEventListener('activate', function (e) {
 
 
     //listen for fetch event, log url of req
-  self.addEventListener('fetch', function(e){
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function(request){
-        if(request){ //if cache vailble, res with cache
-          console.log('responding with cache: '+ e.request.url)
-          return request
-        } else { // no cache, try fetching req
-          console.log('file id not caches, fetching : '+ e.request.url)
-          return fetch(e.request)
+    self.addEventListener('fetch', function(evt) {
+      if (evt.request.url.includes('/api/')) {
+          evt.respondWith(
+            caches
+              .open(DATA_CACHE_NAME)
+              .then(cache => {
+                return fetch(evt.request)
+                  .then(response => {
+                    // If the response was good, clone it and store it in the cache.
+                    if (response.status === 200) {
+                      cache.put(evt.request.url, response.clone());
+                    }
+      
+                    return response;
+                  })
+                  .catch(err => {
+                    // Network request failed, try to get it from the cache.
+                    return cache.match(evt.request);
+                  });
+              })
+              .catch(err => console.log(err))
+          );
+      
+          return;
         }
-      })
-    );
+        evt.respondWith(
+          fetch(evt.request).catch(function() {
+            return caches.match(evt.request).then(function(response) {
+              if (response) {
+                return response;
+              } else if (evt.request.headers.get('accept').includes('text/html')) {
+                // return the cached home page for all requests for html pages
+                return caches.match('/');
+              }
+            });
+          })
+        );
   });
+  
+  
