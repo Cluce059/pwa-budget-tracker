cc04c80b95bbf412d5c3cd3135c1744f7cda7944
caroline <cluce059@gmail.com>
2021-10-21 20:09:07 -0400

    cleaning up

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,62 +1,46 @@
 let db;
 
-// establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
+// connect to idb db called 'budget_tracker' and set it to v 1
 const request = indexedDB.open('budget_tracker', 1);
-
-// this event will emit if the database version changes
+// if version changes
 request.onupgradeneeded = function(event) {
-
-    // save a reference to the database
     const db = event.target.result;
-
     // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
-// upon a successful
 request.onsuccess = function(event) {
-
-    // when db is successfully created with its object store or simply established a connection, save reference to db in global variable
+    // save db ref if successful
     db = event.target.result;
-
-    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
     if (navigator.onLine) {
-        // todo: uploadTransaction();
+        // checkDB();
     }
 };
 
 request.onerror = function(event) {
-    // log error here
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+// submit transaction with no connection
 function saveRecord(record) {
-
-    // open a new transaction with the database with read and write permissions
+    // open a new transaction with db
     const transaction = db.transaction(['new_transaction'], 'readwrite');
-
     // access the object store for `new_transaction`
     const  budgetObjectStore = transaction.objectStore('new_transaction');
-
     // add record to your store with add method
     budgetObjectStore.add(record);
 }
 
-function uploadTransaction() {
-
+function checkDB() {
     // open a transaction on your db
     const transaction = db.transaction(['new_transaction'], 'readwrite');
-
-    // access your object store
+    // access object store
     const budgetObjectStore = transaction.objectStore('new_transaction');
-
-    // get all records from store and set to a variable
+    // get all records from store 
     const getAll = budgetObjectStore.getAll();
 
     getAll.onsuccess = function() {
-
-        // if there was data in indexedDb's store send it to the api server
+        // if there was data in store send to api 
         if (getAll.result.length > 0) {
             fetch('/api/transaction', {
                 method: 'POST',
@@ -91,5 +75,5 @@ function uploadTransaction() {
 }
 
 // listen for app coming back online
-window.addEventListener('online', uploadTransaction);
+window.addEventListener('online', checkDB);
 
